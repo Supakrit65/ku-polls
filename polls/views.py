81c04b13604b0dda4f68f8791d9a218408f27a9b
Supakrit65 <supakrit.tan65@hotmail.com>
2022-09-11 15:24:22 +0700

    redirect visitors to index when can't vote

--- polls/views.py
+++ polls/views.py
@@ -4,6 +4,7 @@ from django.urls import reverse
 from django.views import generic
 from django.utils import timezone
 from .models import Choice, Question
+from django.contrib import messages
 
 
 class IndexView(generic.ListView):
@@ -29,6 +30,19 @@ class DetailView(generic.DetailView):
         Excludes any questions that aren't published yet.
         """
         return Question.objects.filter(pub_date__lte=timezone.localtime())
+
+    def get(self, request, *args, **kwargs):
+        self.object = get_object_or_404(Question, pk=kwargs['pk'])
+        # If someone navigates to a poll detail page when voting is not allowed,
+        # redirect them to the polls index page
+        if not self.object.is_published():
+            messages.error(request, 'That given question is not published yet.')
+            return HttpResponseRedirect(reverse('polls:index'))
+        elif not self.object.can_vote():
+            messages.error(request, 'This question is closed.')
+            return HttpResponseRedirect(reverse('polls:results', args=(self.object.id,)))
+        else:
+            return super().get(request, *args, **kwargs)
 
 
 class ResultsView(generic.DetailView):
