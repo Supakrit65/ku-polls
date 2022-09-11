8938a1a93eba870a94600ff22686332570cd2ffc
Supakrit65 <supakrit.tan65@hotmail.com>
2022-09-11 21:12:35 +0700

    Done docstring models and views

--- polls/models.py
+++ polls/models.py
@@ -7,14 +7,20 @@ from django.contrib import admin
 
 
 class Question(models.Model):
-    """This class represents a model of question
-    with some functional methods."""
+    """
+    This class represents a model of question contains choices.
+    """
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
     end_date = models.DateTimeField('date ended', null=True, blank=True)
 
     def __str__(self) -> str:
-        """Return a text of a question."""
+        """Return a text of a question.
+
+        :param self: Question object.
+
+        :returns: text of a given question.
+        """
         return str(self.question_text)
 
     @admin.display(
@@ -23,17 +29,32 @@ class Question(models.Model):
         description='Published recently?',
     )
     def was_published_recently(self) -> bool:
-        """Check if the question is published within one day."""
+        """Check if the question is published within one day.
+
+        :param self: Question object.
+
+        :returns: the question is published within a day or not.
+        """
         now = timezone.localtime()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
     def is_published(self) -> bool:
-        """Check if the question is published."""
+        """Check if the question is published.
+
+        :param self: Question object.
+
+        :returns: the question is published or not.
+        """
         now = timezone.localtime()
         return now >= self.pub_date
 
     def can_vote(self) -> bool:
-        """Check if the question can be voted."""
+        """Check if the question can be voted.
+
+        :param self: Question object.
+
+        :returns: can the question be voted.
+        """
         now = timezone.localtime()
         if not self.end_date:
             return self.is_published()
@@ -41,11 +62,16 @@ class Question(models.Model):
 
 
 class Choice(models.Model):
-    """This class is a subclass models.Model."""
+    """This class has a ForeignKey as a Question."""
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
 
     def __str__(self) -> str:
-        """Return a test of a choice."""
+        """Return a text of a choice.
+
+        :param self: Choice object.
+
+        :returns: text of a given choice.
+        """
         return str(self.choice_text)
--- polls/views.py
+++ polls/views.py
@@ -67,7 +67,9 @@ class ResultsView(generic.DetailView):
 
 
 def vote(request, question_id):
-    """Handle voting"""
+    """
+    Increment a vote count for selected choice.
+    """
     question = get_object_or_404(Question, pk=question_id)
     try:
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
