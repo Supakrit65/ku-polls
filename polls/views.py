83258705c35a2b11f910d872380e39b14ca293cd
Supakrit65 <supakrit.tan65@hotmail.com>
2022-09-11 17:12:38 +0700

    Improve navigation between pages.

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, Http404
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
@@ -32,17 +32,22 @@ class DetailView(generic.DetailView):
         return Question.objects.filter(pub_date__lte=timezone.localtime())
 
     def get(self, request, *args, **kwargs):
-        self.object = get_object_or_404(Question, pk=kwargs['pk'])
+        try:
+            self.object = get_object_or_404(Question, pk=kwargs['pk'])
+        except Http404:
+            messages.error(request, 'No such question.')
+            return HttpResponseRedirect(reverse('polls:index'))
         # If someone navigates to a poll detail page when voting is not allowed,
         # redirect them to the polls index page
-        if not self.object.is_published():
-            messages.error(request, 'That given question is not published yet.')
-            return HttpResponseRedirect(reverse('polls:index'))
-        elif not self.object.can_vote():
-            messages.error(request, 'This question is closed.')
-            return HttpResponseRedirect(reverse('polls:results', args=(self.object.id,)))
         else:
-            return super().get(request, *args, **kwargs)
+            if not self.object.is_published():
+                messages.error(request, 'That given question is not published yet.')
+                return HttpResponseRedirect(reverse('polls:index'))
+            elif not self.object.can_vote():
+                messages.error(request, 'This question is closed.')
+                return HttpResponseRedirect(reverse('polls:results', args=(self.object.id,)))
+            else:
+                return super().get(request, *args, **kwargs)
 
 
 class ResultsView(generic.DetailView):
