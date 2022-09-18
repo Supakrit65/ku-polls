3c70a3f999479dc6608d52c4b96c5896a682d0be
Supakrit65 <supakrit.tan65@hotmail.com>
2022-09-18 14:51:20 +0700

    add signup

--- mysite/urls.py
+++ mysite/urls.py
@@ -13,13 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import include, path
 from django.shortcuts import redirect
+from . import views
 
 urlpatterns = [
     path('', lambda request: redirect('polls/')),
     path('polls/', include('polls.urls')),
     path("admin/", admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('signup/', views.signup, name='signup')
 ]
